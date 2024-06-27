e609d97d07d5014a1442960e637ffea279986da6
Diogo <dfmcordeiro@gmail.com>
2024-06-27 15:38:22 +0100

    next.config.js

---
+++ my-app/next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Your existing config here (if any)
+  
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  
+    // Any other configurations you might have
+  };
